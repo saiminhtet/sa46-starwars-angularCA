8cfa1e971d852ec271b898444bf1668d67de158f
saiminhtet <htetsaimin@gmail.com>
2019-01-20 22:36:14 +0800

    update ngsw-config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { SwUpdate } from '@angular/service-worker';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'STARWARS';
+
+  constructor(update: SwUpdate) {
+    update.available.subscribe( event => {
+      update.activateUpdate().then(() => document.location.reload());
+    });
+  }
 }
